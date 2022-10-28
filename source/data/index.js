44e7711280e5b6b7f02e777d7314b4b7fba0397b
Денис <Allimp_94@mail.ru>
2022-10-28 21:55:28 +0300

    Начало шапки и футера

--- source/data/index.js
+++ source/data/index.js
@@ -1,1 +1,37 @@
-export default {};
+export default {
+  contacts: [
+    {
+      id: 'mailto:',
+      value: 'info@sd.ru'
+    },
+    {
+      id: 'tel:',
+      value: '+74950005252',
+      title: '+7 (495) 000 52 52'
+    }
+  ],
+
+  callBack: {
+    title: 'Заказать звонок',
+    url: '#!'
+  },
+
+  slogan: 'умные технологии',
+  socials: [
+    {
+      id: 'ok',
+      value: '#!',
+      title: 'Одноклассники'
+    },
+    {
+      id: 'vk',
+      value: '#!',
+      title: 'В контакте'
+    },
+    {
+      id: 'pinterest',
+      value: '#!',
+      title: 'Пинктерест'
+    }
+  ]
+};
