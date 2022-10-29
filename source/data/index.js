1b81d1f2c76aeed2d2d29fde670f4251ae09dc0c
Денис <Allimp_94@mail.ru>
2022-10-29 23:18:07 +0300

    Промо

--- source/data/index.js
+++ source/data/index.js
@@ -2,18 +2,18 @@ export default {
   contacts: [
     {
       id: 'mailto',
-      value: 'info@sd.ru'
+      value: 'info@sd.ru',
     },
     {
       id: 'tel',
       value: '+74950005252',
-      title: '+7 (495) 000 52 52'
-    }
+      title: '+7 (495) 000 52 52',
+    },
   ],
 
   callBack: {
     title: 'Заказать звонок',
-    url: '#!'
+    url: '#!',
   },
 
   slogan: 'умные технологии',
@@ -21,17 +21,27 @@ export default {
     {
       id: 'ok',
       value: '#!',
-      title: 'Одноклассники'
+      title: 'Одноклассники',
     },
     {
       id: 'vk',
       value: '#!',
-      title: 'В контакте'
+      title: 'В контакте',
     },
     {
       id: 'pinterest',
       value: '#!',
-      title: 'Пинктерест'
-    }
-  ]
+      title: 'Пинктерест',
+    },
+  ],
+  promo: {
+    content:
+      '<h2>Печатные платы</h2><p>производство и монтаж, поставка комплектующих, блоков и модулей</p>',
+    image: {
+      default: 'img/background-promo.jpg',
+      default2x: 'img/background-promo@2x.jpg',
+      webp: 'img/background-promo.webp',
+      webp2x: 'img/background-promo@2x.webp',
+    },
+  },
 };
