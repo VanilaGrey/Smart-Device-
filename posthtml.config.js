3b1b945c8ee6acbadaad44511bea04204675fd2d
Денис <Allimp_94@mail.ru>
2022-12-05 15:19:02 +0300

    типограф

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,15 +1,29 @@
-const { getPosthtmlW3c } = require('pineglade-w3c');
+const {getPosthtmlBemLinter} = require('posthtml-bem-linter');
+const {getPosthtmlW3cValidator} = require('posthtml-w3c-validator');
+const parser = require('posthtml-parser');
+const render = require('posthtml-render');
+const Typograf = require('typograf');
+
+const typograf = new Typograf({
+  locale: ['ru', 'en-US'],
+  htmlEntity: {type: 'name'},
+});
+typograf.disableRule('ru/other/phone-number');
+const typography = () => (tree) => parser(typograf.execute(render(tree)));
+
+const getSourceName = (filename) =>
+  filename.replace(/^.*pages(\\+|\/+)(.*)\.twig$/, '$2').replace(/\\/g, '/');
 
 module.exports = {
   plugins: [
-    getPosthtmlW3c({
+    typography(),
+    getPosthtmlW3cValidator({
       exit: process.env.NODE_ENV === 'development',
       forceOffline: true,
-      getSourceName(filename) {
-        return filename
-          .replace(/^.*pages(\\+|\/+)(.*)\.twig$/, '$2')
-          .replace(/\\/g, '/');
-      }
-    })
-  ]
+      getSourceName,
+    }),
+    getPosthtmlBemLinter({
+      getSourceName,
+    }),
+  ],
 };
